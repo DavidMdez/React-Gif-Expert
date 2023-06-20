8144626afc3d3562650b002e6427201363750a27
DavidMdez <mdez.hdez.gamaliel@gmail.com>
2023-06-20 10:24:47 -0600

    custom hook

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,34 +1,32 @@
 import { useState } from "react";
 import { AddCategory } from "./components/AddCategory";
+import { GifGrid } from "./components/GifGrid";
 
 export const GifExpertApp = () => {
-  const [categories, setCategories] = useState(['One Punch', 'Dragon Ball']);
+  const [categories, setCategories] = useState(['One Punch']);
   
   const onAddCategory = ( newCategory ) => {
-
     if( categories.includes(newCategory) ) return;
-    
+
     setCategories( [ newCategory, ...categories ] )
   }
 
   return (
   <>
-    {/* titulo */}
     <h1>GifExpertApp</h1>
 
-    {/* Input */}
     <AddCategory
       onNewCategory={ onAddCategory }
     />
 
-    {/* Listado de Gif */}
-    <ol>
-      { categories.map( category => {
-          return <li key={ category } >{ category }</li>
-        })
-      }
-    </ol>
-      {/* Gif Item */}
+    { categories.map( category => (
+          <GifGrid 
+            key={ category } 
+            category={ category } 
+          />
+        )
+      )
+    }
   </>
   )
 }
