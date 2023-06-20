0acf12312d56f2954d6e043fa969e8b44dd63110
DavidMdez <mdez.hdez.gamaliel@gmail.com>
2023-06-19 19:55:39 -0600

    Add styless global

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,6 +1,14 @@
 
 export const GifExpertApp = () => {
   return (
+  <>
+    {/* titulo */}
     <div>GifExpertApp</div>
+
+    {/* Input */}
+
+    {/* Listado de Gif */}
+      {/* Gif Item */}
+  </>
   )
 }
